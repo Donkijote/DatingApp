f70c5195f36eda76b321956779983b241e592163
Donkijote <manuelmjgc@gmail.com>
2019-11-12 16:23:52 -0300

    creating new repository for users

---
+++ DatingApp-api/Data/IDatingRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp_api.Models;
+
+namespace DatingApp_api.Data
+{
+    public interface IDatingRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+    }
+}
--- DatingApp-api/Startup.cs
+++ DatingApp-api/Startup.cs
@@ -39,6 +39,7 @@ namespace DatingApp_api
             services.AddControllers();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
