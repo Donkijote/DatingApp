57d464c72b00de04966d3e720153faa4024b7533
Donkijote <manuelmjgc@gmail.com>
2019-11-17 21:01:39 -0300

    finishing off the member edit component

--- DatingApp-spa/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-spa/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertsService } from 'src/app/_services/alerts.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -18,7 +20,12 @@ export class MemberEditComponent implements OnInit {
       $event.returnValue = true;
     }
   }
-  constructor(private route: ActivatedRoute, private alert: AlertsService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private alert: AlertsService,
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -28,8 +35,16 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alert.success('Profile updated successfully');
-    this.editForm.reset(this.user);
+    this.userService
+      .updateUser(this.authService.decodedToken.nameid, this.user)
+      .subscribe(
+        next => {
+          this.alert.success('Profile updated successfully');
+          this.editForm.reset(this.user);
+        },
+        error => {
+          this.alert.error(error);
+        }
+      );
   }
 }
