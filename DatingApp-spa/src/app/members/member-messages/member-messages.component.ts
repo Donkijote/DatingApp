54cac7c1526260ceb47d330c835675bd1550ea53
Donkijote <manuelmjgc@gmail.com>
2019-11-29 21:59:23 -0300

    adding mark as Read functionallity

--- DatingApp-spa/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-spa/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from 'src/app/_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertsService } from 'src/app/_services/alerts.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -25,8 +26,21 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.auth.decodedToken.nameid;
     this.userService
       .getMessageThread(this.auth.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (
+              messages[i].isRead === false &&
+              messages[i].recipientId === currentUserId
+            ) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
       .subscribe(
         resp => {
           this.messages = resp;
