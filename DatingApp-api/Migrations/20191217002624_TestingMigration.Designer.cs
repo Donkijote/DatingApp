634ef716100b47a69cad458f1dadfa17b6d3ee4d
Donkijote <manuelmjgc@gmail.com>
2019-12-16 21:34:14 -0300

    testing again dropping and creating database

--- DatingApp-api/Migrations/20191217002624_TestingMigration.Designer.cs
+++ DatingApp-api/Migrations/20191217002624_TestingMigration.Designer.cs
@@ -4,14 +4,16 @@ using DatingApp_api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp_api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191217002624_TestingMigration")]
+    partial class TestingMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
